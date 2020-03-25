2f80e455ad017962d629cdca6c32174d1a5ccf39
amindazad <amindazad@icloud.com>
2020-03-24 17:49:38 -0700

    createdthe Database and added User and Text

--- tweetkhan/__init__.py
+++ tweetkhan/__init__.py
@@ -1,3 +1,5 @@
 """ Entry point for TwitKhan Flask application. """
+from .app import create_app
 
+APP = create_app()
 
--- tweetkhan/app.py
+++ tweetkhan/app.py
@@ -1,13 +1,15 @@
 """Main application and routing logic for twitkhan"""
-
 from flask import Flask
+from .models import DB
 
 def create_app():
-    """Create and configure an instance of the Flask application"""\
+    """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "Welcome to TwitKhan!"
+        return "Welcome to TweetKhan!🧞"
         
     return app
